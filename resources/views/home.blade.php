ad0ce7aae0ecde26f4ceadb3f93b20686fde26eb
Stanghi <stangherlin.matti@gmail.com>
2023-01-09 17:23:14 +0100

    add table movie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,28 @@
 @extends('layout.main')
 
 @section('content')
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Original title</th>
-                <th scope="col">Nationality</th>
-                <th scope="col">Date</th>
-                <th scope="col">Vote</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($movies as $movie)
+    <div class="container mt-5">
+        <table class="table">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $movie->title }}</th>
-                    <td>{{ $movie->original_title }}</td>
-                    <td>{{ $movie->nationality }}</td>
-                    <td>{{ $movie->date }}</td>
-                    <td>{{ $movie->vote }}</td>
+                    <th scope="col">Title</th>
+                    <th scope="col">Original title</th>
+                    <th scope="col">Nationality</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Vote</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($movies as $movie)
+                    <tr>
+                        <th scope="row">{{ $movie->title }}</th>
+                        <td>{{ $movie->original_title }}</td>
+                        <td>{{ $movie->nationality }}</td>
+                        <td>{{ $movie->date }}</td>
+                        <td>{{ $movie->vote }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
